a73b791b9424521ce68506a0ea3a34f3da8af473
carlosortegatoro <carlosortegatoro@gmail.com>
2017-09-18 10:09:04 +0200

    Adding post

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const throng = require('throng');
 
-const WORKERS = process.env.WEB_CONCURRENCY || 250;
+const WORKERS = process.env.WEB_CONCURRENCY || 5;
 const PORT = process.env.PORT || 5000;
 const BLITZ_KEY = process.env.BLITZ_KEY;
 
