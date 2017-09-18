b0651021f8cc428d6cdfb7826b46f9d759890556
carlosortegatoro <carlosortegatoro@gmail.com>
2017-09-18 10:04:44 +0200

    Adding Post

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const throng = require('throng');
 
-const WORKERS = process.env.WEB_CONCURRENCY || 500;
+const WORKERS = process.env.WEB_CONCURRENCY || 250;
 const PORT = process.env.PORT || 5000;
 const BLITZ_KEY = process.env.BLITZ_KEY;
 
@@ -16,38 +16,19 @@ function start() {
   const app = express();
 
   app
-    .use(blitz(BLITZ_KEY))
-    .get('/cpu', cpuBound)
-    .get('/memory', memoryBound)
-    .get('/io', ioBound)
     .get('/get', getResponse)
+    .port('/post', postResponse)
     .listen(PORT, onListen);
 
-  function cpuBound(req, res, next) {
-    const key = Math.random() < 0.5 ? 'ninjaturtles' : 'powerrangers';
-    const hmac = crypto.createHmac('sha512WithRSAEncryption', key);
-    const date = Date.now() + '';
-    hmac.setEncoding('base64');
-    hmac.end(date, () => res.send('A hashed date for you! ' + hmac.read()));
-  }
-
-  function memoryBound(req, res, next) {
-    const hundredk = new Array(100 * 1024).join('X');
-    setTimeout(function sendResponse() {
-      res.send('Large response: ' + hundredk);
-    }, 300).unref();
-  }
-
-  function ioBound(req, res, next) {
-    setTimeout(function SimulateDb() {
-      res.send('Got response from fake db!');
-    }, 300).unref();
-  }
 
   function getResponse(req, res, next) {
   	setTimeout(function SimulateDb() {
       res.json({ message : 'everything is ok'});
     },300).unref();
+  }
+
+  function postResponse(req, res, next) {
+    res.json({ message : 'Post went OK'});
   }
 
   function onListen() {
